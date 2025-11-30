b4d54b234f82d18810dca7bdb2843c0e290355b5
Finger15945 <roofing15945@gmail.com>
2025-11-30 22:49:02 +0700

    fix lagi darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // PENTING: Agar switch manual berfungsi
+  darkMode: 'class', // PENTING: Agar tombol switch berfungsi
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -15,7 +15,8 @@ export default {
       },
       colors: {
         brand: {
-          // Syntax ini memanggil variable dari file style.css
+          // JANGAN UBAH INI KE HEX KODE MANUAL
+          // Biarkan dia membaca variabel dari style.css
           bg: 'rgb(var(--color-bg) / <alpha-value>)',
           surface: 'rgb(var(--color-surface) / <alpha-value>)',
           text: 'rgb(var(--color-text) / <alpha-value>)',
@@ -26,6 +27,7 @@ export default {
       },
       boxShadow: {
         'soft': '0 4px 20px -2px rgba(0, 0, 0, 0.1)',
+        'glow': '0 0 15px rgba(45, 212, 191, 0.3)',
       }
     },
   },
